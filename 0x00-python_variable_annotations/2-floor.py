45074039d7fc25d7512625f78f928cf15481983e
ronsig30 <ronaldrogony30@gmail.com>
2024-08-02 02:45:29 +0300

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of a float value.
+
+    Parameters:
+    n (float): The float value to get the floor of.
+
+    Returns:
+    int: The largest integer less than or equal to the float value.
+    """
+    return math.floor(n)
