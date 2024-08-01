649778beade6b5f4cad190ecd1b8c5158a6fa9e3
ronsig30 <ronaldrogony30@gmail.com>
2024-08-02 02:42:31 +0300

    Basic annotation to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to its string representation.
+
+    Parameters:
+    n (float): The float to be converted to a string.
+
+    Returns:
+    str: The string representation of the float.
+    """
+    return str(n)
