0c5b1d279300724f3e43a59bf4a779d18a8fafec
ronsig30 <ronaldrogony30@gmail.com>
2024-08-02 02:40:02 +0300

    Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import Literal
+
+# Define the variables with their specified values and types
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
